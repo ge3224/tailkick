7e804ad911318aed62ac9e4667c5cc1a51bcc2d0
ge3224 <ge3224@gmail.com>
2022-12-02 09:07:27 -0600

    modify css; add front-page.php

---
+++ dist/front-page.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+<?php get_header(); ?>
+
+<body>
+  <?php get_template_part('nav_primary', get_post_format()); ?>
+
+this is my test
+  <?php get_footer(); ?>
+  <script src="<?php bloginfo('template_url'); ?>/js/tailkick1.js"></script>
+</body>
+
+</html>
