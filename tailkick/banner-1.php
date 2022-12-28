88bb108edc0d82061b51cd36230fdb02f01e0f79
ge3224 <ge3224@gmail.com>
2022-12-28 08:56:00 -0600

    Bug fix: push renamed template-part file

--- tailkick/banner-1.php
+++ tailkick/banner-1.php
@@ -1,14 +1,18 @@
+<?php
+ /**
+ * A full-width banner for TailKick
+ *
+ * @package WordPress
+ * @subpackage TailKick
+ */
+?>
+
 <section id="call-to-action" class="mb-8">
   <div class="mx-auto max-w-6xl h-96 bg-gray-100 bg-[url('/wp-content/themes/tailkick/images/tk-cta-1.jpg')] bg-cover bg-center rounded-sm px-8 flex flex-col space-y-3 justify-center">
     <div class="max-w-xs">
       <h3 class="font-bold text-3xl text-white">Dare to be Different</h3>
       <p class="text-white">Lorem ipsum dolor sit amet consectetur. Interdum turpis enim sodales orci nullam turpis.</p>
-      <a
-        type="button"
-        class="mt-3 px-4 py-2 font-bold bg-teal-300 border border-black shadow-[5px_5px_0_0] shadow-teal-700/30"
-        style="background-color: <?php echo get_theme_mod('global_color_primary'); ?> ;"
-        href="https://github.com/ge3224/tailkick"
-      >Download</a>
+      <?php get_template_part('button', null, array('text' => 'Get Started', 'link' => 'https://github.com/ge3224/tailkick', 'target' => '_blank')); ?>
     </div>
   </div>
 </section>
