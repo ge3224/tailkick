4e4982ce62548f5af545bbbf501c3ee94bf546a2
ge3224 <ge3224@gmail.com>
2022-12-29 14:33:17 -0600

    Remove from front-page some experimental code

--- tailkick/front-page.php
+++ tailkick/front-page.php
@@ -38,25 +38,6 @@
     <?php if (get_theme_mod('showcase_panel_include') == "true") : ?>
       <?php get_template_part('template-parts/showcase-panel', get_post_format()); ?>
     <?php endif; ?>
-    <!-- testing zone -->
-    <?php
-      global $wp_registered_widgets;
-      foreach ($wp_registered_widgets as $key => $value) {
-        echo '<h2>' . $key . '</h2>';
-        echo '<p>name => '. $wp_registered_widgets[$key]['name'] . '</p>';
-        echo '<p>id => '. $wp_registered_widgets[$key]['id'] . '</p>';
-        echo '<p>callback => '. $wp_registered_widgets[$key]['callback'] . '</p>';
-        echo '<p>params => '. $wp_registered_widgets[$key]['params'] . '</p>';
-        echo '<p>classname => '. $wp_registered_widgets[$key]['classname'] . '</p>';
-        echo '<p>customize_selective_refresh => '. $wp_registered_widgets[$key]['customize_selective_refresh'] . '</p>';
-        echo '<p>description => '. $wp_registered_widgets[$key]['description'] . '</p>';
-        echo '<p>show_instance_in_rest => '. $wp_registered_widgets[$key]['show_instance_in_rest'] . '</p>';
-        foreach($value as $k => $v) {
-          echo '<p>' . $k . ' => ' . $v . '</p>';
-        }
-      }
-    ?>
-    <!-- end of testing zone -->
     <?php get_template_part('template-parts/feat-1', get_post_format()); ?>
     <?php get_template_part('template-parts/feat-2', get_post_format()); ?>
     <?php get_template_part('template-parts/feat-3', get_post_format()); ?>
