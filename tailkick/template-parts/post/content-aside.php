d4d2110ce6d742f548102db998fc6c019bd3b47c
ge3224 <ge3224@gmail.com>
2023-01-23 14:29:10 -0600

    Tweak aside post format

--- tailkick/template-parts/post/content-aside.php
+++ tailkick/template-parts/post/content-aside.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Template part for displaying posts
  *
@@ -13,50 +14,50 @@
 ?>
 
 <article id="post-<?php the_ID(); ?>" <?php post_class('mb-8'); ?>>
-	<?php
-	if ( is_sticky() && is_home() ) :
-		echo tailkick_get_svg( array( 'icon' => 'thumb-tack' ) );
-	endif;
-	?>
+  <?php
+  if (is_sticky() && is_home()) :
+    echo tailkick_get_svg(array('icon' => 'thumb-tack'));
+  endif;
+  ?>
 
-	<div class="border border-gray-g00 bg-none rounded-sm pt-4 px-4 pb-0.5 entry-content">
+  <div class="border border-gray-g00 bg-none rounded-sm pt-4 px-4 pb-0.5 entry-content">
 
-    <?php if ( '' !== get_the_post_thumbnail() ) : ?>
+    <?php if (get_the_post_thumbnail() !== '') : ?>
       <div class="mb-3 w-full max-h-96 overflow-hidden post-thumbnail">
         <a href="<?php the_permalink(); ?>">
-          <?php the_post_thumbnail( 'tailkick-featured-image', array ( 'class' => 'object-cover object-center') ); ?>
+          <?php the_post_thumbnail('tailkick-featured-image', array('class' => 'object-cover object-center')); ?>
         </a>
       </div>
     <?php endif; ?>
 
     <div>
 
-    <div class="w-full flex items-center mb-2">
-      <div class="mr-2 w-10 h-10 rounded-full overflow-hidden bg-gray-300">
-        <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
-          <?php echo get_avatar(get_the_author_meta('ID'), 40, 'mm', 'Author Avatar'); ?>
-        </a>
-      </div>
-      <div>
-        <div class="text-base my-0 py-0">
-          <a class="font-bold text-black hover:text-gray-600 active:text-gray-500" href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
-            <?php the_author(); ?>
+      <div class="w-full flex items-center mb-2">
+        <div class="mr-2 w-10 h-10 rounded-full overflow-hidden bg-gray-300">
+          <a href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+            <?php echo get_avatar(get_the_author_meta('ID'), 40, 'mm', 'Author Avatar'); ?>
           </a>
         </div>
-        <div class="text-xs text-gray-500 mr-2 my-0 py-0"> 
-          <span><?php echo tailkick_time_link(); ?></span>
-        </div> 
+        <div>
+          <div class="text-base my-0 py-0">
+            <a class="font-bold text-black hover:text-gray-600 active:text-gray-500" href="<?php echo get_author_posts_url(get_the_author_meta('ID')); ?>">
+              <?php the_author(); ?>
+            </a>
+          </div>
+          <div class="text-xs text-gray-500 mr-2 my-0 py-0">
+            <span><?php echo tailkick_time_link(); ?></span>
+          </div>
+        </div>
+        <div class="ml-auto mr-0"><?php tailkick_edit_link(); ?></div>
       </div>
-      <div class="ml-auto mr-0"><?php tailkick_edit_link(); ?></div>
-    </div>
 
-    <?php
-      echo '<a href="'. esc_url( get_permalink() ) . '" rel="bookmark">';
-      
+      <?php
+      echo '<a href="' . esc_url(get_permalink()) . '" rel="bookmark">';
+
       the_content(
         sprintf(
           /* translators: %s: Post title. Only visible to screen readers. */
-          __( 'Continue reading<span class="' . sr_only_classes( array('screen-reader-text') ) . '">"%s"</span>', 'tailkick' ),
+          __('Continue reading<span class="' . sr_only_classes(array('screen-reader-text')) . '">"%s"</span>', 'tailkick'),
           get_the_title()
         )
       );
@@ -64,23 +65,21 @@
 
       wp_link_pages(
         array(
-          'before'      => '<div class="page-links">' . __( 'Pages:', 'tailkick' ),
+          'before'      => '<div class="page-links">' . __('Pages:', 'tailkick'),
           'after'       => '</div>',
           'link_before' => '<span class="page-number">',
           'link_after'  => '</span>',
         )
       );
-		?>
+      ?>
 
     </div>
-	</div>
+  </div>
 
-	<?php
-	if ( is_single() ) {
-		tailkick_entry_footer();
-	}
-	?>
+  <?php
+  if (is_single()) {
+    tailkick_entry_footer();
+  }
+  ?>
 
 </article>
-
-
