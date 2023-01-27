7dc9eafba99bf5e187a36677229658f8fb003d33
ge3224 <ge3224@gmail.com>
2023-01-27 13:25:50 -0600

    Handle responsiveness of front-page hero section

--- tailkick/front-page.php
+++ tailkick/front-page.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * The front page template file
  *
@@ -21,7 +22,8 @@
  * override Tailwind styles. To fallback to Tailwind styling, modifications
  * made in the Customize API must be be 'cleared.'
  */
-function get_hero_bg_image_mods() {
+function get_hero_bg_image_mods()
+{
   // See ./inc/tk_customize.php
   $img = get_theme_mod('hero_home_image');
   $x = get_theme_mod('hero_home_image_position_x');
@@ -33,13 +35,13 @@ function get_hero_bg_image_mods() {
     if ($img != '') {
       $output .= 'background: url(\'' . $img . '\') no-repeat';
 
-      if ( $x != "" ) {
-        $output .= ' '. $x;
+      if ($x != "") {
+        $output .= ' ' . $x;
       } else {
         $output .= ' center';
       }
 
-      if ( $y != "" ) {
+      if ($y != "") {
         $output .= ' ' . $y . ';';
       } else {
         $output .= ' center;';
@@ -50,62 +52,62 @@ function get_hero_bg_image_mods() {
   return $output;
 }
 
-?> 
+?>
 
 <!DOCTYPE html>
 <html class="h-full no-js no-svg" <?php language_attributes(); ?>>
 <?php get_header(); ?>
 
 <body <?php body_class('min-h-full flex flex-col'); ?>>
-<?php wp_body_open(); ?>
-<div id="page" class="site">
-	<a class="<?php echo sr_only_classes( array('skip-link', 'screen-reader-text') ); ?>" href="#content"><?php _e( 'Skip to content', 'tailkick' ); ?></a>
-	<header id="masthead" class="site-header">
-		<?php if ( has_nav_menu( 'primary' ) ) : ?>
-			<div class="navigation-top">
-				<div class="relative wrap">
-					<?php get_template_part( 'template-parts/navigation/navigation', 'primary' ); ?>
-				</div>
-			</div>
-		<?php endif; ?>
-    <section id="hero" class="bg-gray-200 bg-hero-home bg-cover bg-center lg:bg-[center_top_35%] bg-no-repeat lg:h-2/3 xl:h-[<?php echo get_theme_mod('home_hero_height', '48.5rem'); ?>] w-full" <?php echo get_hero_bg_image_mods(); ?>>
-      <div class="w-full h-[767px] max-w-6xl mx-auto flex flex-col justify-center items-start">
-        <div class="w-1/5 ml-auto mr-0">
-          <h1 class="text-6xl font-bold"><?php echo get_theme_mod('hero_home_heading', 'Buy. Sell. Discover.'); ?></h1>
-          <p class="mt-3"><?php echo get_theme_mod('hero_home_text', 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum luctus gravida neque, et fringilla erat aliquet id.'); ?></p>
-          <a class="<?php echo tailkick_button_classes_primary(); ?>" href="https://github.com/ge3224/tailkick" target="_blank" type="button"<?php echo get_custom_styles_btn(); ?>>Download</a>
+  <?php wp_body_open(); ?>
+  <div id="page" class="site">
+    <a class="<?php echo sr_only_classes(array('skip-link', 'screen-reader-text')); ?>" href="#content"><?php _e('Skip to content', 'tailkick'); ?></a>
+    <header id="masthead" class="site-header">
+      <?php if (has_nav_menu('primary')) : ?>
+        <div class="navigation-top">
+          <div class="relative wrap">
+            <?php get_template_part('template-parts/navigation/navigation', 'primary'); ?>
+          </div>
         </div>
-      </div>
-    </section>
-	</header>
-	<div class="py-6 max-w-6xl mx-auto site-content-contain">
-		<div id="content" class="site-content">
-      <div id="primary" class="content-area">
-        <main id="main" class="site-main">
+      <?php endif; ?>
+      <section id="hero" class="bg-gray-200 bg-hero-home bg-cover bg-center bg-no-repeat lg:bg-[center_top_35%] w-full lg:h-2/3 xl:h-[<?php echo get_theme_mod('home_hero_height', '48.5rem'); ?>]" <?php echo get_hero_bg_image_mods(); ?>>
+        <div class="w-full h-[767px] max-w-6xl mx-auto flex flex-col justify-end md:justify-center items-start p-5 xl:p-0">
+          <div class="md:w-1/5 lg:m:1/4 md:ml-auto md:mr-6 lg:mr-12 xl:mr-0">
+            <h1 class="text-4xl lg:text-5xl xl:text-6xl font-bold"><?php echo get_theme_mod('hero_home_heading', 'Buy. Sell. Discover.'); ?></h1>
+            <p class="mt-3"><?php echo get_theme_mod('hero_home_text', 'Interdum et malesuada fames ac ante ipsum primis in faucibus. Vestibulum luctus gravida neque, et fringilla erat aliquet id.'); ?></p>
+            <a class="<?php echo tailkick_button_classes_primary(); ?>" href="https://github.com/ge3224/tailkick" target="_blank" type="button" <?php echo get_custom_styles_btn(); ?>>Download</a>
+          </div>
+        </div>
+      </section>
+    </header>
+    <div class="py-6 max-w-6xl mx-auto site-content-contain">
+      <div id="content" class="site-content">
+        <div id="primary" class="content-area">
+          <main id="main" class="site-main">
 
-          <?php
+            <?php
 
             // Display the showcase panel unless disabled in the Customize API
-            if (get_theme_mod('showcase_panel_include') == "true") : 
-              get_template_part('template-parts/page/content', 'front-page-showcase'); 
-            endif; 
-
-            // Show the selected front page content.
-            if ( have_posts() ) :
-              while ( have_posts() ) :
-                the_post();
-                get_template_part( 'template-parts/page/content', 'front-page' );
-              endwhile;
-            else :
-              get_template_part( 'template-parts/post/content', 'none' );
+            if (get_theme_mod('showcase_panel_include') == "true") :
+              get_template_part('template-parts/page/content', 'front-page-showcase');
             endif;
 
-          ?>
+            // Show the selected front page content.
+            if (have_posts()) :
+              while (have_posts()) :
+                the_post();
+                get_template_part('template-parts/page/content', 'front-page');
+              endwhile;
+            else :
+              get_template_part('template-parts/post/content', 'none');
+            endif;
 
-          <?php
-            
+            ?>
+
+            <?php
+
             // Get each of our panels and show the post data.
-            if ( 0 !== tailkick_panel_count() || is_customize_preview() ) : // If we have pages to show.
+            if (0 !== tailkick_panel_count() || is_customize_preview()) : // If we have pages to show.
 
               /**
                * Filters the number of front page sections in TailKick.
@@ -114,25 +116,26 @@ function get_hero_bg_image_mods() {
                *
                * @param int $num_sections Number of front page sections.
                */
-              $num_sections = apply_filters( 'tailkick_front_page_sections', 4 );
+              $num_sections = apply_filters('tailkick_front_page_sections', 4);
               global $tailkickcounter;
 
               // Create a setting and control for each of the sections available in the theme.
-              for ( $i = 1; $i < ( 1 + $num_sections ); $i++ ) {
+              for ($i = 1; $i < (1 + $num_sections); $i++) {
                 $tailkickcounter = $i;
-                tailkick_front_page_section( null, $i );
+                tailkick_front_page_section(null, $i);
               }
 
             endif;
 
-          ?>
+            ?>
 
-        </main>
+          </main>
+        </div>
       </div>
-		</div>
-	</div>
-  <?php get_footer(); ?>
-</div>
+    </div>
+    <?php get_footer(); ?>
+  </div>
 
 </body>
+
 </html>
