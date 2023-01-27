23f050aacc15b942ccb023cedce65436c186a20f
ge3224 <ge3224@gmail.com>
2023-01-27 13:44:11 -0600

    Handle responsiveness of front-page showcase panel

--- tailkick/template-parts/page/content-front-page-showcase.php
+++ tailkick/template-parts/page/content-front-page-showcase.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Showcase panel for the TailKick WordPress theme
  *
@@ -7,26 +8,32 @@
  */
 ?>
 
-<section class="mx-auto max-w-6xl w-full flex justify-between py-14 space-x-16" id="showcase_panel">
-  <div class="flex flex-col justify-center align-middle">
-    <div class="w-28 mx-auto">
-      <img class="w-full h-auto" src="<?php echo get_theme_mod('showcase_box1_img', get_bloginfo('template_url').'/assets/images/tk-heart-ico.png'); ?>">
+<section class="mx-auto max-w-6xl w-full flex flex-col md:flex-row justify-between px-4 xl:px-0 py-14 space-y-6 md:space-y-0 md:space-x-16" id="showcase_panel">
+  <div class="flex md:flex-col justify-center items-center md:items-start align-middle">
+    <div class="flex-shrink-0 w-24 md:w-28 mx-auto">
+      <img class="w-full h-auto" src="<?php echo get_theme_mod('showcase_box1_img', get_bloginfo('template_url') . '/assets/images/tk-heart-ico.png'); ?>">
     </div>
-    <h3 class="mt-3 font-bold text-3xl text-center"><?php echo get_theme_mod('showcase_box1_heading', 'Show It'); ?></h3>
-    <p class="mt-3"><?php echo get_theme_mod('showcase_box1_text', 'Duis nec ante lorem. Ut vestibulum nibh id auctor semper. Etiam consectetur accumsan dui sed malesuada.'); ?></p>
+    <div class="ml-6 md:ml-0">
+      <h3 class="mt-3 font-bold text-3xl md:text-center"><?php echo get_theme_mod('showcase_box1_heading', 'Show It'); ?></h3>
+      <p class="mt-3"><?php echo get_theme_mod('showcase_box1_text', 'Duis nec ante lorem. Ut vestibulum nibh id auctor semper. Etiam consectetur accumsan dui sed malesuada.'); ?></p>
+    </div>
   </div>
-  <div class="flex flex-col justify-center align-middle">
-    <div class="w-28 mx-auto">
-      <img class="w-full h-auto" src="<?php echo get_theme_mod('showcase_box2_img', get_bloginfo('template_url').'/assets/images/tk-boombox-ico.png'); ?>">
+  <div class="flex md:flex-col justify-center items-center md:items-start align-middle">
+    <div class="flex-shrink-0 w-24 md:w-28 mx-auto">
+      <img class="w-full h-auto" src="<?php echo get_theme_mod('showcase_box2_img', get_bloginfo('template_url') . '/assets/images/tk-boombox-ico.png'); ?>">
     </div>
-    <h3 class="mt-3 font-bold text-3xl text-center"><?php echo get_theme_mod('showcase_box2_heading', 'Sing It'); ?></h3>
-    <p class="mt-3"><?php echo get_theme_mod('showcase_box2_text', 'Curabitur ut ligula at turpis efficitur auctor elementum sed risus. Morbi egestas consectetur suscipit. Sed vitae lobortis purus.'); ?></p>
+    <div class="ml-6 md:ml-0">
+      <h3 class="mt-3 font-bold text-3xl md:text-center"><?php echo get_theme_mod('showcase_box2_heading', 'Sing It'); ?></h3>
+      <p class="mt-3"><?php echo get_theme_mod('showcase_box2_text', 'Curabitur ut ligula at turpis efficitur auctor elementum sed risus. Morbi egestas consectetur suscipit. Sed vitae lobortis purus.'); ?></p>
+    </div>
   </div>
-  <div class="flex flex-col justify-center align-middle">
-    <div class="w-28 mx-auto">
-      <img class="w-full h-auto" src="<?php echo get_theme_mod('showcase_box3_img', get_bloginfo('template_url').'/assets/images/tk-check-ico.png'); ?>">
+  <div class="flex md:flex-col justify-center items-center md:items-start align-middle">
+    <div class="flex-shrink-0 w-24 md:w-28 mx-auto">
+      <img class="w-full h-auto" src="<?php echo get_theme_mod('showcase_box3_img', get_bloginfo('template_url') . '/assets/images/tk-check-ico.png'); ?>">
     </div>
-    <h3 class="mt-3 font-bold text-3xl text-center"><?php echo get_theme_mod('showcase_box3_heading', 'Share It'); ?></h3>
-    <p class="mt-3"><?php echo get_theme_mod('showcase_box3_text', 'Quisque efficitur finibus nibh sit amet varius. Etiam ante purus, ullamcorper vitae massa vel, ornare euismod sapien.'); ?></p>
+    <div class="ml-6 md:ml-0">
+      <h3 class="mt-3 font-bold text-3xl md:text-center"><?php echo get_theme_mod('showcase_box3_heading', 'Share It'); ?></h3>
+      <p class="mt-3"><?php echo get_theme_mod('showcase_box3_text', 'Quisque efficitur finibus nibh sit amet varius. Etiam ante purus, ullamcorper vitae massa vel, ornare euismod sapien.'); ?></p>
+    </div>
   </div>
 </section>
